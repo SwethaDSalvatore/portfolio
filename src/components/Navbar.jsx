88c412b8e52b5edce6f340f1307f6b3dba623574
SwethaDSalvatore <swetharamesh35309@gmail.com>
2024-03-06 21:20:16 +0530

    fix: navbar position updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='p-5 shadow bg-slate-900 border-b border-rose-400 flex items-center justify-between fixed w-full'>
+    <div className='p-5 shadow bg-slate-900 border-b border-rose-400 flex items-center justify-between fixed top-0 w-full z-10'>
       <h1 className='text-rose-400 font-custom font-semibold text-xl'>Swetha Ramesh</h1>
       <button className="px-4 py-2 bg-rose-400  hover:bg-rose-500 rounded-lg">
-              <a
-                href="https://swethadsalvatore.github.io/Swetha.Ramesh-Resume/"
-                target="_blank"
-                className="font-semibold"
-              >
-                Hire me
-              </a>
-            </button>
+        <a
+          href="https://swethadsalvatore.github.io/Swetha.Ramesh-Resume/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-semibold"
+        >
+          Hire me
+        </a>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
